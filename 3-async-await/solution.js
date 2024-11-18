5c437b639fd5f3c82a6a04b8140ba25d76d6f7f2
franc1sc0sv <franc1sc0.sv.xd@gmail.com>
2024-11-18 13:56:10 -0600

    Francisco Hernandez - Challengues

--- 3-async-await/solution.js
+++ 3-async-await/solution.js
@@ -1,35 +1,73 @@
-/*
-INSTRUCTIONS
+import products from "./products.js";
+import prices from "./prices.js";
 
-1. using async/await API consume products and prices methods
-2. don't use .then(), .catch() or .finally() here
-3. both, products and prices methods expect a positive integer id
-4. use Promise.all() and Promise.allSettled() to consume both methods in parallel
-5. to generate the id do the following: invoke Date.now(), and take the last two digits, this will be your id
-6. log the results with console.log(), the format is up to you, but it must include id, product and price
+const generateID = () => new Date().getTime().toString().slice(-2);
 
-Example:
-{
- id:100,
- product:'paper',
- price:1
+async function solution() {
+  const ID = generateID();
+
+  // Promises
+  const promises = [products(ID), prices(ID)];
+
+  // Promise.All
+  try {
+    const [productName, productPrice] = await Promise.all(promises);
+    console.log(`(Promise.All):`, {
+      id: ID,
+      product: productName,
+      price: productPrice,
+    });
+  } catch (error) {
+    console.log(`Error (Promise.All): ${error.message || "Unknown error"}`);
+  }
+
+  // Promise.allSettled
+  const resultsAllSettled = await Promise.allSettled(promises);
+
+  const allFulfilled = resultsAllSettled.every(
+    (result) => result.status === "fulfilled"
+  );
+
+  if (allFulfilled) {
+    const productName = resultsAllSettled[0].value;
+    const productPrice = resultsAllSettled[1].value;
+
+    console.log(`(Promise.AllSettled):`, {
+      id: ID,
+      product: productName,
+      price: productPrice,
+    });
+  }
+
+  if (!allFulfilled) {
+    console.log(
+      `Error (Promise.allSettled): ${
+        resultsAllSettled[0].reason || "Unknown error"
+      }`
+    );
+  }
+
+  // Promise.race -- Returns the first promise to be resolve o reject and finishing all the others (In this case always is going to be product promise It's the first in the array )
+  try {
+    const firstResolved = await Promise.race(promises);
+    console.log(`First resolved (Promise.race):`, {
+      id: ID,
+      data: firstResolved,
+    });
+  } catch (error) {
+    console.log(`Error (Promise.race): ${error.message || "Unknown error"}`);
+  }
+
+  //Promise.any() -- Returns the first to be resolve successfully
+  try {
+    const firstFulfilled = await Promise.any(promises);
+    console.log(`First fulfilled (Promise.any):`, {
+      id: ID,
+      data: firstFulfilled,
+    });
+  } catch (error) {
+    console.log(`Error (Promise.any): ${error.errors || "Unknown error"}`);
+  }
 }
 
-7. both methods include some conditions to fail, at the end you should console.log() the errors, the format is up to you
-8. add any needed adjustment to solution() function
-9. as extra challenge: add Promise.race() and Promise.any(), and try to get the idea of what happens
-*/
-
-function solution() {
-    // YOUR SOLUTION GOES HERE
-
-    // You generate your id value here
-
-    // You use Promise.all() here
-
-    // You use Promise.allSettled() here
-
-    // Log the results, or errors, here
-}
-
-solution()
+solution();
